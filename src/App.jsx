c239c86b57f5b7d6c1e3f277e754f5aa4cbb8cd8
Rayana <rayanatxr@gmail.com>
2025-04-25 17:52:21 -0300

    Adiciona Navbar e Footer globalmente no layout do aplicativo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Home from './pages/home/Home.jsx';
+import Home from './pages/home/home.jsx';
 import Catalogo from './components/layout/catalogo/Catalog.jsx';
 import CatalogoCompleto from './components/layout/catalogocompleto/Catalogocompleto.jsx';
 import MapExploracao from './components/layout/mapexploration/MapExploration.jsx';
 import LoginModal from './components/auth/login/LoginModal.jsx';
 import CadastroModal from './components/auth/signup/SignupModal.jsx';
 import PainelAdmin from './pages/admin/PainelAdmin.jsx';
-import AdicionarEvento from './pages/admin/AddEvent.jsx'
+import AdicionarEvento from './pages/admin/AddEvent.jsx';
 import FavoritosPage from './pages/favoritespage/eventsfav.jsx';
 import Sobre from './pages/sobre/sobrenos.jsx';
-import ForgotPassword from './components/auth/forgotpassword/ForgotPassword.jsx'  
+import ForgotPassword from './components/auth/forgotpassword/ForgotPassword.jsx';  
 import Navbar from './components/layout/navbar/navbar.jsx';
+import Footer from './components/layout/footer/footer.jsx'; // Importando Footer
 import { EventsProvider } from './components/EventsProvider.jsx';
 import { AuthProvider, useAuth } from './context/AuthContext.jsx';
 import { FavoritesProvider } from './context/FavoritesContext.jsx';
@@ -23,7 +24,6 @@ function ProtectedRoute({ element, requiredRole }) {
   const { user, loading, error } = useAuth();
   
   if (loading) return <div>Carregando...</div>;
-  
   if (error) return <div>Ocorreu um erro. Tente novamente mais tarde.</div>;
   
   const hasAccess = requiredRole === 'admin'
@@ -40,7 +40,7 @@ function App() {
         <QueryClientProvider client={queryClient}>
           <EventsProvider>
             <BrowserRouter>
-              <Navbar />
+              <Navbar /> {/* Navbar exibida em todas as páginas */}
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/catalogo" element={<Catalogo />} />
@@ -54,6 +54,7 @@ function App() {
                 <Route path="/admin/adicionar" element={<ProtectedRoute element={<AdicionarEvento />} requiredRole="admin" />} />
                 <Route path="/favoritos" element={<ProtectedRoute element={<FavoritosPage />} requiredRole="user" />} />
               </Routes>
+              <Footer /> {/* Footer exibido em todas as páginas */}
             </BrowserRouter>
           </EventsProvider>
         </QueryClientProvider>
